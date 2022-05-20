dbba9fab9ea046969e9b67ee0a038982417583ff
Pavel Goncharov <p.goncharov@dunice.net>
2022-05-20 17:14:15 +0300

    work on the function of getting all the news 20/05/22

--- src/components/allNews/AllNews.js
+++ src/components/allNews/AllNews.js
@@ -3,24 +3,18 @@ import axios from "axios";
 import { News } from "./News";
 
 export const AllNews = () => {
-  const [allnews, setAllnews] = useState([
-    {title:"aaaa",
-    text:"ccccc",
-     author:"sssss",
-     tags:"xcxcxc"}])
+  const [allnews, setAllnews] = useState([])
   const link = `http://localhost:5000`
-  // useEffect(() => {
-  //   getAllNews();
-  // }, []);
-  // const getAllNews = async () => {
-  //   try {
-  //     await axios.get(`${link}/news`).then((res) => {
-  //       setAllnews(res.data);
-  //     });
-  //   } catch (e) {
-  //     alert(e.message);
-  //   }
-  // };
+  useEffect(async () => {
+    try {
+      await axios.get(`${link}`).then((res) => {
+      console.log(res.data);
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  }, []);
+
   return (
     <div>
       {allnews.map((element, index) => <News element={element} key={index} />
